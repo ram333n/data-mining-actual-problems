65c5a0b43760ba1a829338e29d66423d3622d577
Roman Prokopchuk <ghost62727@gmail.com>
2025-02-09 16:01:34 +0200

    Implement One Rule classifier

---
+++ Lab_1/classifiers/one_rule.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class OneRuleClassifier:
+    def __init__(self):
+        self.best_feature = None
+        self.rules = None
+
+    def fit(self, X, y):
+        self.__validate_fit_values(X, y)
+
+        min_error = 1
+        best_feature_idx = None
+        best_rules = None
+
+        for f_idx in range(X.shape[1]):
+            f_column = X[:, f_idx]
+            cur_rules = self.__create_rules(f_column, y)
+            cur_error = self.__evaluate_error(f_column, y, cur_rules)
+
+            if cur_error < min_error:
+                min_error = cur_error
+                best_feature_idx = f_idx
+                best_rules = cur_rules
+
+        self.best_feature = best_feature_idx
+        self.rules = best_rules
+
+    def __validate_fit_values(self, X, y):
+        data_rows_count = X.shape[0]
+        labels_rows_count = y.shape[0]
+
+        if data_rows_count != labels_rows_count:
+            raise ValueError('Dimension mismatch with data and label rows')
+
+    def __create_rules(self, f_column, y):
+        rules = {}
+
+        for f_val in np.unique(f_column):
+            most_frequent_class = self.__find_the_most_frequent_class(f_column, f_val, y)
+            rules[f_val] = most_frequent_class
+
+        return rules
+
+    def __find_the_most_frequent_class(self, f_column, f_val, y):
+        values, counts = np.unique(y[f_column == f_val], return_counts=True)
+        return values[np.argmax(counts)]
+
+    def __evaluate_error(self, f_column, y, rules):
+        predictions = np.array([rules[x] for x in f_column])
+        return np.sum(predictions != y) / y.shape[0]
+
+    def predict(self, X):
+        f_column = X[:, self.best_feature]
+
+        return np.array([self.rules[f_val] for f_val in f_column])
+
--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,7 +1,10 @@
+import numpy as np
 import pandas as pd
 
 from classifiers.knn import KNNClassifier
 from classifiers.naive_bayes import NaiveBayesClassifier
+from classifiers.one_rule import OneRuleClassifier
+
 
 def read_dataset(filename):
     df = pd.read_csv(filename)
@@ -14,9 +17,9 @@ def test_naive_bayes():
     bayes_classifier = NaiveBayesClassifier(0.1)
     bayes_classifier.fit(X, y)
 
-    X_test = [
+    X_test = np.array([
         [2, 1, 1, 1]
-    ]
+    ])
     print(f'Predicted class: {bayes_classifier.predict(X_test)}')
 
 def test_knn():
@@ -24,10 +27,20 @@ def test_knn():
     knn_classifier = KNNClassifier(3, weighted_voting=False)
     knn_classifier.fit(X, y)
 
-    X_test = [
+    X_test = np.array([
         [2, 1, 1, 1]
-    ]
+    ])
     print(f'Predicted class: {knn_classifier.predict(X_test)}')
 
+def test_one_rule():
+    X, y = read_dataset('./data/input_0.csv')
+    one_rule_classifier = OneRuleClassifier()
+    one_rule_classifier.fit(X, y)
+
+    X_test = np.array([
+        [2, 1, 1, 1],
+    ])
+    print(f'Predicted class: {one_rule_classifier.predict(X_test)}')
+
 if __name__ == '__main__':
-    test_knn()
+    test_one_rule()
